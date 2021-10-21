ce8b05554f3179cf0720ea567aab1c7beb9ca02b
Don Moses <githinjidon@gmail.com>
2021-10-21 15:54:30 +0300

    add urls for new views added

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,11 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('', views.home, name='home'),
     re_path(r'^profile/(\d+)', views.profile, name="profile"),
+    re_path(r'^create/post', views.new_post, name="new-post"),
+    re_path(r'^follow/(\d+)', views.follow, name="follow"),
+    re_path(r'^create/comment/(\d+)', views.new_comment, name="new_comment"),
+    re_path(r'^like/(\d+)', views.like, name="like"),
+    re_path(r'^post/(\d+)', views.post, name="post"),
 ]
 
 if settings.DEBUG:
